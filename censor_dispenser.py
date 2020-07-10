75f5d08c1186692eaaa431c90d5431da0e4a69be
Tom Wang <t.wang604@gmail.com>
2020-07-10 01:34:39 -0400

    Update censor_dispenser.py

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,3 +1,5 @@
+# CA Challenge project prompt!
+
 # These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
 email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
